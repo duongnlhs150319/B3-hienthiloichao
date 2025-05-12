0adc2690447d46aa9974976a805a670c5eaaf4fe
duongnlhs150319 <duongnlhs150319@gmail.com>
2025-05-13 01:42:08 +0700

    Hien thi loi chao

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhập tên của bạn vào: ");
+        string yourName = Console.ReadLine();
+        Console.WriteLine("Xin chào: " + yourName);
+    }
+}
